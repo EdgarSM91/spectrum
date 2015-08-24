c96b5c3ffe705685fa0f862305efd53d0430dc66
root <root@localhost.localdomain>
2015-08-24 15:26:07 -0500

    cambios

--- apps/modules/frontend/controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Modules\Frontend\Controllers;
-class IndexController extends ControllerBase
-{
-    public function indexAction(){
-    }
-    public function contactAction(){
-    }
-}
